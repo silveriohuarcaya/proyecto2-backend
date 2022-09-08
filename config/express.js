2e8a1fa998990b57880695f07e540236f1bba184
silverio huarcaya <corwilgi@gmail.com>
2022-09-08 10:52:04 -0500

    fix deployment cors

--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 
 function configExpress(app) {
-    app.use(express.json());
-    app.use(morgan('dev'));
+  app.use(cors());
+  app.use(express.json());
+  app.use(morgan('dev'));
 }
 module.exports = configExpress;
