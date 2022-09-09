4cd7c579239c4a54235e69c087917bf48f682ae9
silverio huarcaya <corwilgi@gmail.com>
2022-09-09 08:48:57 -0500

    fix express

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+
 const connectDB = require('./config/database');
 const configExpress = require('./config/express');
 const configRoutes = require('./routes');
 
 const app = express();
 
-connectDB();
 configExpress(app);
+connectDB();
 configRoutes(app);
 
 PORT = process.env.PORT || 8080;
