e0ad141acc54cb3a9f9e42a560e91b878f3dd4df
Silverio Huarcaya <corwilgi@gmail.com>
2022-10-18 16:27:46 -0500

    feaature:fix

--- api/preference/index.js
+++ api/preference/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
-const { getAllPreferenceHandler, createPreferenceHandler } = require('./preference.controller');
+const { getAllPreferenceHandler, createPreferenceHandler, createPreferenceWebhok } = require('./preference.controller');
 
 const router = Router();
 
 router.get('/orders', getAllPreferenceHandler);
 router.post('/orders', createPreferenceHandler);
+router.post('/', createPreferenceWebhok);
 
 
 module.exports = router;
