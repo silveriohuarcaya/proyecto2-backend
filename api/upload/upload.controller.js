3082ea417b507b25ebfe09bff1a7b630e2b10afe
Silverio Huarcaya <corwilgi@gmail.com>
2022-11-08 09:57:45 -0500

    fix:scss

--- api/upload/upload.controller.js
+++ api/upload/upload.controller.js
@@ -3,19 +3,18 @@ const cloudinary = require('cloudinary').v2;
 
 async function uploadSingleHandler(req, res) {
   const { file } = req;
-  console.log('silverio backend file 2:', file);
-  const size = file.size / 1024 / 1024; // MB
+  // const size = file.size / 1024 / 1024; // MB
 
-  if (size > 2) {
-    return res.status(400).json({ message: 'File size is too big' });
-  }
+  // if (size > 2) {
+  //   return res.status(400).json({ message: 'File size is too big' });
+  // }
 
   try {
     const result = await cloudinary.uploader.upload(file.path, {
       folder: 'assets',
-      // use_filename: true,
-      // unique_filename: false,
-      // overwrite: true,
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
       resource_type: 'auto',
     });
     return res.json(result);
